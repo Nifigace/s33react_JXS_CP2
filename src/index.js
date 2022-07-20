7bb231f1f112c6aa47948a1431be9b614a78038a
Nifigace <53969580+Nifigace@users.noreply.github.com>
2022-07-20 18:17:15 +0300

    done challenge

--- src/index.js
+++ src/index.js
@@ -6,3 +6,21 @@
 //E.g.
 //Created by Angela Yu.
 //Copyright 2019.
+import React from "react";
+import ReactDOM from "react-dom";
+
+const fName = "Roman";
+const lName = "Lisyuk";
+const num = Math.floor(Math.random() * 10);
+let cYear = new Date().getFullYear();
+
+ReactDOM.render(
+  <div>
+    <h1>What's up with you?</h1>
+    <p>
+      Created by {`${fName} ${lName}`} &copy; {cYear}
+    </p>
+    <p></p>
+  </div>,
+  document.getElementById("root")
+);

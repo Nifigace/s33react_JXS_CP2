3cf61399716ae2aef337ba4a4cf45b62364a6785
Nifigace <53969580+Nifigace@users.noreply.github.com>
2022-07-20 21:31:29 +0300

    images didn't show up but worked

--- src/index.js
+++ src/index.js
@@ -11,16 +11,24 @@ import ReactDOM from "react-dom";
 
 const fName = "Roman";
 const lName = "Lisyuk";
-const num = Math.floor(Math.random() * 10);
 let cYear = new Date().getFullYear();
+const img = "https://picsum.photos/200";
 
 ReactDOM.render(
   <div>
-    <h1>What's up with you?</h1>
-    <p>
-      Created by {`${fName} ${lName}`} &copy; {cYear}
-    </p>
-    <p></p>
+    <h1 className="heading">What's up with you?</h1>
+    <p>Created by {`${fName} ${lName}`}</p>
+    <p>&copy; {cYear}</p>
+    <br />
+    <h1></h1>
+    <ul>
+      <li>one one one one</li>
+      <li>two two two two</li>
+      <li>three three three</li>
+    </ul>
+    <div>
+      <img scr={`img + "?grayscale"`} />
+    </div>
   </div>,
   document.getElementById("root")
 );
